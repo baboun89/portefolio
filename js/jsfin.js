11b0c146d60ffafefcf544a5d15ba41fb4b4dd7d
baboun89 <philbab@live.fr>
2021-09-16 13:32:01 +0200

    stop ecoute

--- js/jsfin.js
+++ js/jsfin.js
@@ -1,5 +1,7 @@
 let point = document.querySelector(".point")
 
-point.addEventListener("click", function () {
+point.addEventListener("click",del)
+function del() {
      sessionStorage.setItem('click', 1)
-})
+     point.removeEventListener("click",del)
+}
